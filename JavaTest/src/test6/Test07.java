67c5bb125e7989d56c5d00ca8016a782dde37771
eternalsoul0634 <eternalsoul0634@gmail.com>
2025-07-24 17:47:51 +0900

    수정

--- JavaTest/src/test6/Test07.java
+++ JavaTest/src/test6/Test07.java
@@ -7,6 +7,42 @@ package test6;
  * 
  */
 
+class Vehicle {
+	private String kind;
+	private int fee;
+	??;
+	
+	public void take(??) {
+		System.out.printf("%s은 %를 탑니다.\n", user.getName(), kind);
+		System.out.printf("%s요금은 %,d입니다.\n", kind, fee);
+		
+		
+	}
+}
+
+class User {
+	private String name;
+	??;
+	
+	public String getName() {
+		return name;		
+	}
+	public void take(??) {
+		vehicle.take(this);		
+	}
+	
+}
+
 public class Test07 {
+	public static void main(String[] args) {
+		User kim = new User("김유신");
+		User lee = new User("이순신");
+		
+		Vehicle vehicle1 = new Vehicle("버스", 1500);
+		Vehicle vehicle2 = new Vehicle("택시", 3600);
+		
+		kim.take(vehicle1);
+		lee.take(vehicle2);
+	}
 
 }
--- JavaTest/src/test6/Test09.java
+++ JavaTest/src/test6/Test09.java
@@ -7,6 +7,44 @@ package test6;
  * 
  */
 
+enum Gender {
+	MALE, FEMALE
+}
+class Man {
+	public void print() {
+		System.out.println("남자 입니다.");
+	}
+}
+class Woman {
+	public void print() {
+		System.out.println("여자 입니다.");
+	}
+}
+
 public class Test09 {
+	
+	public static void main(String[] args) {
+		
+		Object human1 = makeHuman(Gender.MALE);
+		Object human1 = makeHuman(Gender.FEMALE);
+		
+		if(human1 ?? Man) {
+			Man man = (Man) humanl;
+			man.print();			
+		}
+		
+		if(human2 ?? Woman woman) {
+			woman.print();
+		}		
+	}
+	
+	public static ?? makeHuman(Gender gender) {
+		if(gender == Gender.MALE) {
+			return new Man();
+		}else if(gender == Gender.FEMALE) {
+			return new Woman();
+		}
+		return null;
+	}
 
 }
